aeb5786f52b9e5475cb1e395d0eda04b1aa79dac
tianyu5177 <zhangtianyu5177@sina.com>
2020-12-04 11:50:31 +0800

    更新

--- 9_src_最终版/redux/reducers/person.js
+++ 9_src_最终版/redux/reducers/person.js
@@ -6,9 +6,6 @@ export default function (preState=initState,action){
 	const {type,data} = action
 	switch (type) {
 		case ADD_PERSON:
-			// preState.unshift(data)
-			// console.log(preState);
-			// return preState
 			return [data,...preState]
 		default:
 			return preState
